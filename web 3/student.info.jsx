7833f199598c23d75677a340dd29320c730fbbc1
Manav202 <141389168+Manav202@users.noreply.github.com>
2024-05-25 20:36:41 -0600

    Add files via upload

---
+++ web 3/student.info.jsx
@@ -0,0 +1,13 @@
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Manav202</p>
+      <Link href="https://github.com/Manav202/cprg306-assignments.git">
+        Link to your GitHub repository
+      </Link>
+    </div>
+  );
+}
